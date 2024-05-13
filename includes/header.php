a5f5cef881013e08ff3fe9b3a718e2a484e806dc
Yash-Sen <yashsen0402@gmail.com>
2024-05-13 11:46:50 +0530

    Includes Pages Created

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="RealEstate Admin Dashboard template, UI kit, Bootstrap 4x">
+<meta name="author" content="Thememakker">
+<title>:: Oreo RealEstate :: Home</title>
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="<?= urlOf('assets/plugins/bootstrap/css/bootstrap.min.css') ?>">
+<link rel="stylesheet" href="<?= urlOf('assets/plugins/charts-c3/plugin.css') ?>" />
+<link rel="stylesheet" href="<?= urlOf('assets/plugins/jvectormap/jquery-jvectormap-2.0.3.min.css') ?>"/>
+<link rel="stylesheet" href="<?= urlOf('assets/css/main.css') ?>">
+<link rel="stylesheet" href="<?= urlOf('assets/css/color_skins.css') ?>">
+<link rel="stylesheet" href="<?= urlOf('assets/plugins/dropzone/dropzone.css') ?>">
+<link rel="stylesheet" href="<?= urlOf('assets/plugins/jquery-datatable/dataTables.bootstrap4.min.css') ?>">
+<link rel="stylesheet" href="<?= urlOf('assets/plugins/bootstrap-select/css/bootstrap-select.css') ?>"/>
+
+
+</head>
+<body class="theme-purple">
+<!-- Page Loader -->
+<div class="page-loader-wrapper">
+    <div class="loader">
+        <div class="m-t-30"><img class="zmdi-hc-spin" src="https://thememakker.com/templates/oreo/realestate/html/assets/images/logo.svg" width="48" height="48" alt="Oreo"></div>
+        <p>Please wait...</p>        
+    </div>
+</div>
+<!-- Overlay For Sidebars -->
+<div class="overlay"></div>
---
+++ includes/script.php
@@ -0,0 +1,15 @@
+<script src="<?= urlOf('assets/bundles/libscripts.bundle.js') ?>"></script>
+<script src="<?= urlOf('assets/bundles/vendorscripts.bundle.js') ?>"></script>
+<script src="<?= urlOf('assets/bundles/c3.bundle.js') ?>"></script>
+<script src="<?= urlOf('assets/bundles/jvectormap.bundle.js') ?>"></script>
+<script src="<?= urlOf('assets/bundles/knob.bundle.js') ?>"></script>
+<script src="<?= urlOf('assets/bundles/mainscripts.bundle.js') ?>"></script>
+<script src="<?= urlOf('assets/js/pages/index.js') ?>"></script>
+<script src="<?= urlOf('assets/plugins/dropzone/dropzone.js') ?>"></script> 
+<script src="<?= urlOf('assets/bundles/datatablescripts.bundle.js') ?> "></script>
+<script src="<?= urlOf('assets/js/pages/tables/jquery-datatable.js') ?> "></script>
+<script src="<?= urlOf('assets/plugins/jquery-datatable/buttons/dataTables.buttons.min.js') ?>"></script>
+<script src="<?= urlOf('assets/plugins/jquery-datatable/buttons/buttons.bootstrap4.min.js') ?>"></script>
+<script src="<?= urlOf('assets/plugins/jquery-datatable/buttons/buttons.colVis.min.js') ?>"></script>
+<script src="<?= urlOf('assets/plugins/jquery-datatable/buttons/buttons.html5.min.js') ?>"></script>
+<script src="<?= urlOf('assets/plugins/jquery-datatable/buttons/buttons.print.min.js') ?>"></script>
